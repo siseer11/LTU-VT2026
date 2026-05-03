6ebcc4140055b157141123921d41bfb1a029d398
Dragos Cuciureanu <dragos@triggybet.com>
2026-05-03 21:08:22 +0200

    created more methods and splited the code, WIP move files

--- 2-FlowControl/Program.cs
+++ 2-FlowControl/Program.cs
@@ -1,5 +1,8 @@
 ﻿
 
+using System.Reflection.Emit;
+using System.Security.Cryptography.X509Certificates;
+
 bool userClosedTheProgram = false;
 var prices = new Dictionary<PriceCategories, (int price, string description)>
 {
@@ -12,13 +15,13 @@ var menuSelections = new Dictionary<MenuSelections, string>
 {
 	{MenuSelections.CloseApp, "Close App"},
 	{MenuSelections.BuyOneTicket, "Buy one ticket"},
-	{MenuSelections.BuyMultipleTickets, "Buy multiple tickets"}
+	{MenuSelections.BuyGroupTickets, "Buy group tickets"}
 };
 
 do
 {
 	DisplayMainMenu();
-	string invalidMenuSelectionMessage = $"-> Error! Try again, please insert an valid value between 0 - {menuSelections.Count - 1}: ";
+	string invalidMenuSelectionMessage = $"  Please insert an valid value between 0 - {menuSelections.Count - 1}: ";
 	int userMenuSelection = GetValidIntFromUserInput(invalidMenuSelectionMessage, extraIntChecks: (i) => i <= menuSelections.Count - 1);
 
 	switch (userMenuSelection)
@@ -30,27 +33,112 @@ do
 			HandleBuyOneTicket();
 			HandleQuestionAfterCaseHandled(CloseApp);
 			break;
-		case (int)MenuSelections.BuyMultipleTickets:
-			Console.WriteLine("Buy multiple tickets!");
-			Console.ReadLine();
+		case (int)MenuSelections.BuyGroupTickets:
+			HandleBuyGroupTickets();
+			HandleQuestionAfterCaseHandled(CloseApp);
 			break;
 	}
 
 } while (!userClosedTheProgram);
 
-
 void DisplayMainMenu()
 {
 	Console.Clear();
-	Console.WriteLine("========= Main menu =========");
-	Console.WriteLine("\nYou will navigate using numbers,\nthat corespond to the different actions shown below:\n");
-	Console.WriteLine("=============================\n");
+	DisplayMenuHeader(["MAIN MENU", "(navigate by typing a number)"], '=');
 	foreach (var item in menuSelections)
 	{
 		Console.WriteLine($"{(int)item.Key} - {item.Value}");
 	}
-	Console.Write("\nYour choice: ");
+	Console.Write("\nNavigate to: ");
 }
+
+void DisplayMenuHeader(string[] lines, char divider = '-')
+{
+	int longestLine = 20;
+
+	foreach (string line in lines)
+		longestLine = Math.Max(longestLine, line.Length);
+
+	string dividerString = new string(divider, longestLine);
+	Console.WriteLine($"{dividerString}\n");
+	foreach (string line in lines)
+	{
+		int lineLength = line.Length;
+		Console.WriteLine(line.PadLeft((longestLine + lineLength) / 2));
+	}
+	Console.WriteLine($"\n{dividerString}");
+}
+
+
+#region Ask user for confirmation helpers
+T UserConfirmation<T>(string question, List<(string label, T value)> options, Directions direction = Directions.row)
+{
+	if (options == null || options.Count == 0)
+		throw new ArgumentException("Options cannot be empty.");
+
+	bool userSelected = false;
+	int activeIdx = 0;
+
+	Console.WriteLine(question);
+	Console.CursorVisible = false;
+
+	do
+	{
+		foreach (var option in options)
+		{
+			if (options[activeIdx].label == option.label)
+			{
+				Console.BackgroundColor = ConsoleColor.Gray;
+				Console.ForegroundColor = ConsoleColor.Black;
+			}
+			else
+				Console.ResetColor();
+
+			if (direction == Directions.row)
+				Console.Write(option.label);
+			else
+				Console.WriteLine(option.label);
+		}
+		Console.ResetColor();
+
+
+		ConsoleKey[] forwardKeys = [ConsoleKey.RightArrow, ConsoleKey.DownArrow, ConsoleKey.D, ConsoleKey.S, ConsoleKey.Tab];
+		ConsoleKey[] backwardKeys = [ConsoleKey.LeftArrow, ConsoleKey.UpArrow, ConsoleKey.A, ConsoleKey.W];
+
+		ConsoleKey userKeyPressed = Console.ReadKey(true).Key;
+		if (userKeyPressed == ConsoleKey.Enter)
+		{
+			userSelected = true;
+		}
+		else
+		{
+			if (backwardKeys.Contains(userKeyPressed))
+			{
+				activeIdx = activeIdx - 1 < 0 ? options.Count - 1 : activeIdx - 1;
+			}
+			else if (forwardKeys.Contains(userKeyPressed))
+			{
+				activeIdx = (activeIdx + 1) % options.Count;
+			}
+
+			// Reset the cursor, move it at the top of the options, so next time it will write over the old one
+			Console.SetCursorPosition(0, Console.CursorTop - (direction == Directions.row ? 0 : options.Count));
+		}
+
+	} while (!userSelected);
+
+
+	Console.CursorVisible = true;
+	return options[activeIdx].value;
+
+}
+
+bool YesOrNoUserConfirmation(string question)
+{
+	return UserConfirmation(question, [(label: " YES ", value: true), (label: " NO ", value: false)]);
+}
+;
+#endregion
 
 void CloseApp()
 {
@@ -66,7 +154,7 @@ int GetValidIntFromUserInput(string errorMessage = "\nPlease enter a valid integ
 	do
 	{
 		if (int.TryParse(Console.ReadLine(), out result) &&
-				allowNegativeValues ? true : result > 0 &&
+				allowNegativeValues ? true : result >= 0 &&
 				extraIntChecks?.Invoke(result) != false
 			)
 		{
@@ -111,25 +199,23 @@ void HandleQuestionAfterCaseHandled(Action closeApp)
 	return prices[PriceCategories.Standard];
 }
 
+#region Menu handlers
 void HandleBuyOneTicket()
 {
 	Console.Clear();
-	Console.WriteLine("\n--------- Buy one ticket");
-	Console.WriteLine("In order to give you the correct price, we need your age.");
+	DisplayMenuHeader(["BUY ONE TICKET", "(age needed to give you the correct price)"]);
 	Console.Write("\nPlease enter your age: ");
 
 	int customerAge = GetValidIntFromUserInput("\nPlease enter a valid age value: ");
 
 	(int price, string description) = GetTicketPriceWithDescriptionByAge(customerAge);
 
-	Console.WriteLine($"\nBased on your age ({customerAge}), your ticket will be {price}Kr - for a \"{description}\" ticket.");
-
 	/*
 	* Make sure the client wants to get the ticket.
 	*/
-	Console.WriteLine($"\n-> Do you want to proceed with the purchase? You owe us: {price}Kr.\nPress enter to continue with the purchase, anything else to exit.");
+	bool userWantsToBuy = YesOrNoUserConfirmation($"\nBased on your age ({customerAge}), your ticket will be {price}Kr - for a \"{description}\" ticket.\nProceed with the payment?\n");
 
-	if (Console.ReadKey(true).Key == ConsoleKey.Enter)
+	if (userWantsToBuy)
 	{
 		Console.WriteLine("\nEnjoy the movie!");
 	}
@@ -139,6 +225,68 @@ void HandleBuyOneTicket()
 	}
 }
 
+void HandleBuyGroupTickets()
+{
+	Console.Clear();
+	DisplayMenuHeader(["BUY GROUP TICKETS", "(group size and members age needed)"]);
+	Console.Write("Enter the size of the group (min 2 - max 10): ");
+
+	int groupSize = GetValidIntFromUserInput("\nPlease enter a valid group size between 2 - 10: ", extraIntChecks: (i) => i >= 2 && i <= 10);
+	Console.WriteLine($"\n======== Group size {groupSize} ========");
+	Console.WriteLine("Please enter the age of every person in the group, one by one");
+
+	// After the group size was determined, get ages for each client, set price and description acordingly
+	(int age, int price, string description)[] group = new (int age, int price, string description)[groupSize];
+	int totalPrice = 0;
+
+	for (int i = 0; i < groupSize; i++)
+	{
+		Console.Write($"\n-----> Person {i + 1} age: ");
+		int customerAge = GetValidIntFromUserInput($"Please enter a valid age value for the person {i + 1}: ");
+
+		(int price, string description) = GetTicketPriceWithDescriptionByAge(customerAge);
+		totalPrice += price;
+
+		group[i] = (age: customerAge, price, description);
+		Console.WriteLine($"\n+ Person {i + 1} added successfully, subtotal: {totalPrice}Kr\n\n------------------------------");
+	}
+
+	// After we have all the ages for each person in the group, log the total and ask the user if they want to proceed
+	Console.Clear();
+	Console.WriteLine($"\n======== Success ========");
+	Console.WriteLine($"Everyone in the group was added!");
+	Console.WriteLine("Below you can see the detailes and the total");
+
+	Console.WriteLine("\nCustomer\tAge\tTicket\t\tPrice\n");
+	for (int i = 0; i < groupSize; i++)
+	{
+		(int age, int price, string description) = group[i];
+
+		Console.WriteLine($"{i + 1}\t\t{age}\t{description}\t{price}Kr");
+	}
+	Console.WriteLine($"\nTotal:\t\t\t\t\t={totalPrice}Kr");
+
+	Console.WriteLine("---------");
+	Console.WriteLine($"\nIf you want to continue with the purchase (of {totalPrice}Kr) press Enter, any other key to stop it: ");
+
+	if (Console.ReadKey(true).Key == ConsoleKey.Enter)
+	{
+		Console.WriteLine("\nEnjoy the movie!");
+	}
+	else
+	{
+		Console.WriteLine("\nMaybe next time!");
+	}
+	Console.WriteLine("\n================");
+}
+#endregion
+
+#region Enums
+enum Directions
+{
+	row,
+	column
+}
 enum PriceCategories
 {
 	Youth,
@@ -150,5 +298,6 @@ enum MenuSelections
 {
 	CloseApp = 0,
 	BuyOneTicket = 1,
-	BuyMultipleTickets = 2,
+	BuyGroupTickets = 2,
 }
+#endregion
---
+++ trying/MoveArrowsHighlight/Program.cs
@@ -0,0 +1,91 @@
+﻿void MenuUpDown()
+{
+	bool userSelected = false;
+
+	int activeIndex = 0;
+	string[] menuItems = ["Item 1", "Item 2", "Item 3", "Close"];
+
+
+
+	while (!userSelected)
+	{
+		Console.Clear();
+		Console.WriteLine("------------");
+		Console.WriteLine("    Menu    ");
+		Console.WriteLine("------------");
+		foreach (string menuItem in menuItems)
+		{
+			if (menuItems[activeIndex] == menuItem)
+			{
+				Console.BackgroundColor = ConsoleColor.DarkGray;
+				Console.ForegroundColor = ConsoleColor.White;
+
+				Console.WriteLine($" > {menuItem} ");
+			}
+			else
+			{
+				Console.ResetColor();
+				Console.WriteLine(menuItem);
+			}
+			Console.ResetColor();
+		}
+
+		ConsoleKey userPressedKey = Console.ReadKey(true).Key;
+		if (userPressedKey == ConsoleKey.Enter)
+			userSelected = true;
+		else if (userPressedKey == ConsoleKey.UpArrow)
+		{
+			activeIndex = activeIndex - 1 < 0 ? menuItems.Length - 1 : activeIndex - 1;
+		}
+		else if (userPressedKey == ConsoleKey.DownArrow)
+		{
+			activeIndex = (activeIndex + 1) % menuItems.Length;
+		}
+	}
+	Console.WriteLine($"Oh wow, you selected {menuItems[activeIndex]}");
+}
+;
+MenuUpDown();
+// YesAndNo();
+void YesAndNo()
+{
+	bool userSelected = false;
+	string[] selections = [" YES ", " NO "];
+	int activeIdx = 0;
+	Console.WriteLine("THis would be some imporntatn Text,\n Wa wa wi wa!");
+	do
+	{
+		// Console.Clear();
+		Console.SetCursorPosition(0, Console.CursorTop);
+
+		foreach (string selection in selections)
+		{
+			if (selections[activeIdx] == selection)
+			{
+				Console.BackgroundColor = ConsoleColor.Gray;
+				Console.ForegroundColor = ConsoleColor.Black;
+			}
+			else
+				Console.ResetColor();
+
+			Console.Write(selection);
+		}
+		Console.ResetColor();
+		ConsoleKey userKeyPressed = Console.ReadKey(true).Key;
+
+		if (userKeyPressed == ConsoleKey.Enter)
+		{
+			userSelected = true;
+		}
+		else if (userKeyPressed == ConsoleKey.LeftArrow)
+		{
+			activeIdx = Math.Abs(activeIdx - 1);
+		}
+		else if (userKeyPressed == ConsoleKey.RightArrow)
+		{
+			activeIdx = (activeIdx + 1) % selections.Length;
+		}
+
+	} while (!userSelected);
+	Console.WriteLine($"\nOh wow, you selected {selections[activeIdx]}");
+}
